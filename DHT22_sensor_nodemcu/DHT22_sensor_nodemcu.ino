4c58b5b90680d40889b3b58ad089797f3eab749c
jetrich <1139190+jetrich@users.noreply.github.com>
2018-03-27 13:06:48 -0500

    updates
    Added variable.
    Redirected serial output to terminal in blynk app.
    Added serial/terminal notice of read status.

--- DHT22_sensor_nodemcu/DHT22_sensor_nodemcu.ino
+++ DHT22_sensor_nodemcu/DHT22_sensor_nodemcu.ino
@@ -6,16 +6,14 @@
 #include <SimpleTimer.h>;
 #include <DHT.h>;
 
-// You should get Auth Token in the Blynk App.
-// Go to the Project Settings (nut icon).
-char auth[] = "";
-
-// Your WiFi credentials.
-// Set password to "" for open networks.
-char ssid[] = "";
-char pass[] = "";
-
 #define DHTPIN 2 // What digital pin we're connected to
+
+WidgetTerminal terminal(V0); // Set port number for Terminal
+
+char auth[] = "";                // Auth token (Projct Settings page)
+char ssid[] = "";                // Wifi SSID
+char pass[] = "";                // WIFI Password ("" for open network)
+IPAddress blynk_server(X,X,X,X); // IP Address for local blynk server
 
 // Uncomment whatever type you're using!
 //#define DHTTYPE DHT11 // DHT 11
@@ -28,7 +26,7 @@ SimpleTimer timer;
 // This function sends Arduino's up time every second to Virtual Pin (5, 6, 7 &amp;amp;amp;amp;amp;amp; 8).
 // In the app, Widget's reading frequency should be set to PUSH. This means
 // that you define how often to send data to Blynk App.
-void sendSensor()
+void send_sensor()
 {
  float h = dht.readHumidity();
  //float t = dht.readTemperature(); // or dht.readTemperature(true) for Fahrenheit
@@ -44,17 +42,32 @@ void sendSensor()
  Blynk.virtualWrite(V6, t); // Temperature for gauge
  Blynk.virtualWrite(V7, h); // Humidity for graph
  Blynk.virtualWrite(V8, t); // Temperature for graph
+ Serial.print(F("T: ")); Serial.print(t); Serial.print(F(", H: ")); Serial.println(h);
+ }
+
+void sent_serial() {
+  // Sent serial data to Blynk terminal - Unlimited string read
+  String content = ""; // null string constant (empty string)
+  char character;
+  while(Serial.available()) {
+    character = Serial.read();
+    content.concat(character);
+  }
+  if (content != "") {
+    Blynk.virtualWrite (V0, content);
+  }
 }
 
 void setup()
 {
  Serial.begin(115200); // See the connection status in Serial Monitor
- Blynk.begin(auth, ssid, pass, "192.168.56.237", 8080);
+ Blynk.begin(auth, ssid, pass, blynk_server, 8080);
 
  dht.begin();
  
  // Setup a function to be called every second
- timer.setInterval(2000L, sendSensor);
+ timer.setInterval(2000L, send_sensor);
+ timer.setInterval(500L, sent_serial);
 }
 
 void loop()
@@ -62,3 +75,4 @@ void loop()
  Blynk.run(); // Initiates Blynk
  timer.run(); // Initiates SimpleTimer
 }
+
