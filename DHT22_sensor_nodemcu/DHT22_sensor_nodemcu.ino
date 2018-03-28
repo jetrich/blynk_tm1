d17936d1b618ac015ddef7013b822cf4af0a1285
jetrich <1139190+jetrich@users.noreply.github.com>
2018-03-28 15:55:39 -0500

    v.2 commit

--- DHT22_sensor_nodemcu/DHT22_sensor_nodemcu.ino
+++ DHT22_sensor_nodemcu/DHT22_sensor_nodemcu.ino
@@ -1,27 +1,65 @@
 #define BLYNK_PRINT Serial // Comment this out to disable prints and save space
 //#define BLYNK_DEBUG 4
 #include <SPI.h>;
-#include <ESP8266WiFi.h>;
+#include <ESP8266WiFi.h>;           // Use this for WiFi instead of Ethernet.h
 #include <BlynkSimpleEsp8266.h>;
 #include <SimpleTimer.h>;
 #include <DHT.h>;
+#include <MySQL_Connection.h>;
+#include <MySQL_Cursor.h>;
 
 #define DHTPIN 2 // What digital pin we're connected to
 
 WidgetTerminal terminal(V0); // Set port number for Terminal
 
-char auth[] = "";                // Auth token (Projct Settings page)
-char ssid[] = "";                // Wifi SSID
-char pass[] = "";                // WIFI Password ("" for open network)
-IPAddress blynk_server(X,X,X,X); // IP Address for local blynk server
+char debug[] = "no";                              // yes/no additional information via serial for debugging
+
+char auth[] = ""; // Auth token (Projct Settings page)
+char ssid[] = "";                                 // Wifi SSID
+char pass[] = "";                                 // WIFI Password ("" for open network)
+IPAddress blynk_server(X,X,X,X);                  // IP Address for local blynk server
+const int blynk_port = 8080;                      // Specify blynk port
+const int t_gauge = V6;                           // Blynk virtual port for temperature gauge
+const int h_gauge = V5;                           // Blynk virtual port for humidity gauge
+const int t_chart = V8;                           // Blynk virtual port for temperature chart
+const int h_chart = V7;                           // Blynk virtual port for humidity chart
+const int b_term = V0;                            // Blynk virtual port for terminal
+const int sensor_delay = 2000L;                   // Time delay for sensor read (must end with "L")
+const int serial_delay = 500L;                    // Time delay for serial read (must end with "L")
+
+char log_db[] = "yes";                            // yes/no for database logging
+IPAddress db_server(X,X,X,X);                     // Database server IP address
+const int db_port = 3306;                         // Database server port
+char db_user[] = "";                              // Database user
+char db_pass[] = "";                              // Database password
+char db_name[] = "";                              // Database name
+char db_table[] = "";                             // Table name
+WiFiClient client;                                // Use this for WiFi instead of EthernetClient
+MySQL_Connection conn((Client*)&client); 
 
 // Uncomment whatever type you're using!
-//#define DHTTYPE DHT11 // DHT 11
-#define DHTTYPE DHT22 // DHT 22, AM2302, AM2321
-//#define DHTTYPE DHT21 // DHT 21, AM2301
+//#define DHTTYPE DHT11   // DHT 11
+#define DHTTYPE DHT22     // DHT 22, AM2302, AM2321
+//#define DHTTYPE DHT21   // DHT 21, AM2301
 
 DHT dht(DHTPIN, DHTTYPE);
 SimpleTimer timer;
+
+void sent_serial()
+{
+  // Sent serial data to Blynk terminal - Unlimited string read
+  String content = ""; // null string constant (empty string)
+  char character;
+  while(Serial.available()) 
+  {
+    character = Serial.read();
+    content.concat(character);
+  }
+  if (content != "") 
+  {
+    Blynk.virtualWrite (b_term, content);
+  }
+}
 
 void send_sensor()
 {
@@ -32,43 +70,64 @@ void send_sensor()
  //float t = dht.readTemperature(); // Celsius 
  float t = dht.readTemperature(true); // Fahrenheit
 
- if (isnan(h) || isnan(t)) {
- Serial.println("Failed to read from DHT sensor!");
- return;
+ if (isnan(h) || isnan(t)) 
+ {
+  Serial.println("Failed to read from DHT sensor!");
+  return;
  }
  // You can send any value at any time.
  // Please don't send more that 10 values per second.
- Blynk.virtualWrite(V5, h); // Humidity for gauge
- Blynk.virtualWrite(V6, t); // Temperature for gauge
- Blynk.virtualWrite(V7, h); // Humidity for graph
- Blynk.virtualWrite(V8, t); // Temperature for graph
+ Blynk.virtualWrite(h_gauge, h); // Humidity for gauge
+ Blynk.virtualWrite(t_gauge, t); // Temperature for gauge
+ Blynk.virtualWrite(h_chart, h); // Humidity for chart
+ Blynk.virtualWrite(t_chart, t); // Temperature for chart
  Serial.print(F("T: ")); Serial.print(t); Serial.print(F(", H: ")); Serial.println(h); // Feedback via serial
+ if(strcmp(log_db, "yes") == 0)
+ {
+  if(strcmp(debug, "yes") == 0)
+  {
+   Serial.println(F("Log to database: yes"));
+  }
+  char sql_query[255];
+  sprintf(sql_query, "INSERT INTO %s.%s (Temperature, Humidity) VALUES (%f, %f)",db_name,db_table,t,h);
+  if(strcmp(debug, "yes") == 0)
+  {
+   Serial.println(sql_query);
+  }
+  MySQL_Cursor *cur_mem = new MySQL_Cursor(&conn);
+  cur_mem->execute(sql_query);
  }
-
-void sent_serial()
-{
-  // Sent serial data to Blynk terminal - Unlimited string read
-  String content = ""; // null string constant (empty string)
-  char character;
-  while(Serial.available()) {
-    character = Serial.read();
-    content.concat(character);
+ else
+ {
+  if(strcmp(debug, "yes") == 0)
+  {
+   Serial.println(F("Log to database: no"));
   }
-  if (content != "") {
-    Blynk.virtualWrite (V0, content);
-  }
+ }
 }
 
 void setup()
 {
  Serial.begin(115200); // See the connection status in Serial Monitor
- Blynk.begin(auth, ssid, pass, blynk_server, 8080);
+ Blynk.begin(auth, ssid, pass, blynk_server, blynk_port);
+ if(strcmp(log_db, "yes") == 0)
+ {
+  Serial.println("Connecting to SQL... ");
+  if (conn.connect(db_server, db_port, db_user, db_pass)) 
+  {
+    Serial.println("OK."); // Success message
+  }
+ }
+ else 
+ {
+   Serial.println("FAILED!!!!"); // Failure message
+ }
 
  dht.begin();
  
  // Setup a function to be called every second
- timer.setInterval(2000L, send_sensor);
- timer.setInterval(500L, sent_serial);
+ timer.setInterval(sensor_delay, send_sensor);
+ timer.setInterval(serial_delay, sent_serial);
 }
 
 void loop()
